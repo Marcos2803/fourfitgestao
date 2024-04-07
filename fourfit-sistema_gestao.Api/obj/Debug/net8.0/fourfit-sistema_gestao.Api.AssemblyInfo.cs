2546cd09cbe5346725de82fcb6e806125743f07b
Marcos2803 <felipemercadopago@gmail.com>
2024-04-07 20:25:56 -0300

    submit

--- fourfit-sistema_gestao.Api/obj/Debug/net8.0/fourfit-sistema_gestao.Api.AssemblyInfo.cs
+++ fourfit-sistema_gestao.Api/obj/Debug/net8.0/fourfit-sistema_gestao.Api.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("fourfit-sistema_gestao.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b6a0211ea3d322105d3b03b4b9e0016efe889c7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6982007eace1f35f3e4b74d06a1beaa065ddd911")]
 [assembly: System.Reflection.AssemblyProductAttribute("fourfit-sistema_gestao.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("fourfit-sistema_gestao.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- fourfit.sistema_gestao/Factory/DataFactory.cs
+++ fourfit.sistema_gestao/Factory/DataFactory.cs
@@ -14,7 +14,7 @@ namespace fourfit.sistema_gestao.Factory
     {
         public DataContext CreateDbContext(string[] args)
         {
-            var connectionstring = "Data Source=localhost,1401;Initial Catalog=DbSistemaGestao;User ID=sa;Password=@Lab2024;Trust Server Certificate=True";
+            var connectionstring = "Data Source=localhost,1401;Initial Catalog=DbSistemaGestao;User ID=sa;Password=@MLab12366;Trust Server Certificate=True";
             var optionBulder =  new DbContextOptionsBuilder<DataContext>();
             optionBulder.UseSqlServer(connectionstring);
             return new DataContext(optionBulder.Options);
